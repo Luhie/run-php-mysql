476351ddb6d1dd60300c04f957d30fe8bc6ba9d4
Luhie <nerdrun.luhie@gmail.com>
2023-12-18 19:47:06 +0900

    Select multi row

--- select.php
+++ select.php
@@ -6,14 +6,70 @@ $conn = mysqli_connect(
   'opentutorials'
 );
 
+echo '<h1>Single row</h1>';
 $sql = "SELECT * FROM topic WHERE id = 11";
 $result = mysqli_query($conn, $sql);
 if($result){
   $row = mysqli_fetch_array($result);
   // print_r($row);
-  echo '<h1>'.$row['title'].'</h1>';
+  echo '<h2>'.$row['title'].'</h2>';
+  echo '<p>'.$row['description'].'</p>';
+  // var_dump($result->num_rows);
+}
+
+echo '<h1>Multi row</h1>';
+$sql = "SELECT * FROM topic";
+$result = mysqli_query($conn, $sql);
+if($result){
+  $row = mysqli_fetch_array($result);
+  // print_r($row);
+  echo '<h2>'.$row['title'].'</h2>';
   echo '<p>'.$row['description'].'</p>';
   
+  $row = mysqli_fetch_array($result);
+  // print_r($row);
+  echo '<h2>'.$row['title'].'</h2>';
+  echo '<p>'.$row['description'].'</p>';
+
+  $row = mysqli_fetch_array($result);
+  // print_r($row);
+  echo '<h2>'.$row['title'].'</h2>';
+  echo '<p>'.$row['description'].'</p>';
+
+  $row = mysqli_fetch_array($result);
+  // print_r($row);
+  echo '<h2>'.$row['title'].'</h2>';
+  echo '<p>'.$row['description'].'</p>';
   // var_dump($result->num_rows);
+
+  $row = mysqli_fetch_array($result);
+  // print_r($row);
+  echo '<h2>'.$row['title'].'</h2>';
+  echo '<p>'.$row['description'].'</p>';
+  // var_dump($result->num_rows);
+
+
+  $row = mysqli_fetch_array($result);
+  // print_r($row);
+  echo '<h2>'.$row['title'].'</h2>';
+  echo '<p>'.$row['description'].'</p>';
+  // var_dump($result->num_rows);
+
+
+  $row = mysqli_fetch_array($result);
+  // print_r($row);
+  echo '<h2>'.$row['title'].'</h2>';
+  echo '<p>'.$row['description'].'</p>';
+  // var_dump($result->num_rows);
+
+
+  $row = mysqli_fetch_array($result);
+  // print_r($row);
+  echo '<h2>'.$row['title'].'</h2>';
+  echo '<p>'.$row['description'].'</p>';
+  // var_dump($result->num_rows);
+
+  
+  var_dump($row);
 }
 ?>
